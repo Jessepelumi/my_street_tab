ea8790eb4c4f0de33a807164f3444fc289f273fb
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-10-10 13:21:25 +0100

    imported all user screens

---
+++ lib/src/modules/router/navigation_controller.dart
@@ -0,0 +1,19 @@
+import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:get/state_manager.dart';
+import 'package:my_street_tab/src/modules/views/users/chat/chat.dart';
+import 'package:my_street_tab/src/modules/views/users/home/home.dart';
+import 'package:my_street_tab/src/modules/views/users/location/location.dart';
+import 'package:my_street_tab/src/modules/views/users/notification/notification.dart';
+import 'package:my_street_tab/src/modules/views/users/profile/profile.dart';
+
+class NavigationController extends GetxController {
+  final Rx<int> selectedIndex = 0.obs;
+
+  final screens = [
+    const Home(),
+    const Location(),
+    const Chat(),
+    const Notification(),
+    const Profile(),
+  ];
+}
