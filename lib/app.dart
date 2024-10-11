89e5c876ec6130d502226d42643ccf88c2ef66e3
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-10-11 11:28:31 +0100

    changed the app structure to use GetMaterialApp

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,18 @@
 // application structure
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:my_street_tab/src/modules/router/navigation.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "My Street Tab",
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true),
-      home: const Center(
-        child: Text("My Street Tab"),
-      ),
+      home: const Navigation(),
     );
   }
 }
