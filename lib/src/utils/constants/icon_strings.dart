266d12029386da59bd1bc59e7cf7b0b34a2b612b
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-10-14 21:44:00 +0100

    added icons for navigation

---
+++ lib/src/utils/constants/icon_strings.dart
@@ -0,0 +1,14 @@
+class IconStrings {
+  // navigation bar icons
+  static const String chatFilled = "assets/svg/chat-filled.svg";
+  static const String chatOutlined = "assets/svg/chat-outlined.svg";
+  static const String homeFilled = "assets/svg/home-filled.svg";
+  static const String homeOutlined = "assets/svg/home-outlined.svg";
+  static const String locationFilled = "assets/svg/location-filled.svg";
+  static const String locationOutlined = "assets/svg/location-outlined.svg";
+  static const String notificationFilled = "assets/svg/notification-filled.svg";
+  static const String notificationOutlined =
+      "assets/svg/notification-outlined.svg";
+  static const String userFilled = "assets/svg/user-filled.svg";
+  static const String userOutlined = "assets/svg/user-outlined.svg";
+}
