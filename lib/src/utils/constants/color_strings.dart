9302038760f94f423216229b22f713a34495d911
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-10-14 21:50:51 +0100

    created app colors

---
+++ lib/src/utils/constants/color_strings.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const primary = Color(0xFF05145C);
+const primaryTwo = Color(0xFF0B30B2);
+const white = Color(0xFFFFFFFF);
+const darkText = Color(0xFF0A2533);
