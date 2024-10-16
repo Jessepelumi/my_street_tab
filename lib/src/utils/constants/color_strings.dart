398a445006e276f39b34e73ce69e9a422e83779e
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-10-16 21:19:24 +0100

    added color for scaffold

--- lib/src/utils/constants/color_strings.dart
+++ lib/src/utils/constants/color_strings.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 const primary = Color(0xFF05145C);
 const primaryTwo = Color(0xFF0B30B2);
 const white = Color(0xFFFFFFFF);
+const scaffoldWhite = Color(0xFFF5F5F5);
 const darkText = Color(0xFF0A2533);
