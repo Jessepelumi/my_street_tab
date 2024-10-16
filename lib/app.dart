d07681f86bedf535a521531c5fbbc177d2d969b1
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-10-16 21:20:10 +0100

    added custom primary and scaffold colors to theme data

--- lib/app.dart
+++ lib/app.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:my_street_tab/src/modules/router/navigation.dart';
+import 'package:my_street_tab/src/utils/constants/color_strings.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -11,7 +12,11 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData(
+        useMaterial3: true,
+        primaryColor: primary,
+        scaffoldBackgroundColor: scaffoldWhite,
+      ),
       home: const Navigation(),
     );
   }
