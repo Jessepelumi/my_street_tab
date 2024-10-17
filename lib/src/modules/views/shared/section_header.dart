9771f771452499ffa3f33a2853a95a044c2178bd
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-10-17 13:30:24 +0100

    added dynamic properties

--- lib/src/modules/views/shared/section_header.dart
+++ lib/src/modules/views/shared/section_header.dart
@@ -6,7 +6,16 @@ import 'package:my_street_tab/src/utils/core/sizes.dart';
 class SectionHeader extends StatelessWidget {
   const SectionHeader({
     super.key,
+    this.isAction = true,
+    required this.title,
+    this.actionText,
+    this.action,
   });
+
+  final bool isAction;
+  final String title;
+  final String? actionText;
+  final VoidCallback? action;
 
   @override
   Widget build(BuildContext context) {
@@ -14,29 +23,30 @@ class SectionHeader extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Text(
-          "Best Seller",
+          title,
           style: Theme.of(context).textTheme.titleSmall?.copyWith(
                 fontSize: smallLarge,
                 fontWeight: FontWeight.w700,
               ),
         ),
-        Row(
-          children: [
-            GestureDetector(
-              onTap: () {},
-              child: Text(
-                "View All",
-                style: Theme.of(context).textTheme.labelLarge?.copyWith(
-                      fontWeight: FontWeight.w600,
-                    ),
+        if (isAction == true)
+          Row(
+            children: [
+              GestureDetector(
+                onTap: action!,
+                child: Text(
+                  actionText!,
+                  style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                        fontWeight: FontWeight.w600,
+                      ),
+                ),
               ),
-            ),
-            SvgPicture.asset(
-              IconStrings.arrowRight,
-              width: medium,
-            ),
-          ],
-        )
+              SvgPicture.asset(
+                IconStrings.arrowRight,
+                width: medium,
+              ),
+            ],
+          )
       ],
     );
   }
