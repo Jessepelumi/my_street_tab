a8630e8566a5d18fc9d9fbd9fdff3613e25b3b3d
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-10-19 06:00:32 +0100

    extracted the horizontal container

---
+++ lib/src/modules/views/shared/horizontal_container.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:my_street_tab/src/utils/constants/color_strings.dart';
+import 'package:my_street_tab/src/utils/core/sizes.dart';
+
+class HorizontalContainer extends StatelessWidget {
+  const HorizontalContainer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: white,
+        borderRadius: BorderRadius.circular(medium),
+      ),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(medium),
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.3,
+              height: 110,
+              decoration: BoxDecoration(
+                color: primary.withOpacity(0.2),
+                borderRadius: BorderRadius.circular(medium),
+              ),
+            ),
+          ),
+          const SizedBox(width: small),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Cookie Sandwich",
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1,
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleMedium
+                      ?.copyWith(
+                        color: darkText,
+                        fontWeight: FontWeight.w800,
+                      ),
+                ),
+                Text(
+                  "Shortbread, chocolate turtle cookies, and red velvet.",
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                  style: Theme.of(context)
+                      .textTheme
+                      .labelLarge
+                      ?.copyWith(
+                        color: primary.withOpacity(0.5),
+                      ),
+                ),
+                Text(
+                  "\$19.05",
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleMedium
+                      ?.copyWith(
+                        color: red,
+                      ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
