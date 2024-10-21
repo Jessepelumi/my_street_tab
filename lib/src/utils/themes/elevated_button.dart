3f909a2ecde14eabaa89636f60fc292902c7c679
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-10-21 02:40:21 +0100

    created app elevated button theme

---
+++ lib/src/utils/themes/elevated_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:my_street_tab/src/utils/constants/color_strings.dart';
+import 'package:my_street_tab/src/utils/core/sizes.dart';
+
+class AppElevatedButtonTheme {
+  AppElevatedButtonTheme._();
+
+  static final appElevatedButtonTheme = ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: primary,
+      foregroundColor: Colors.white,
+      fixedSize: const Size(double.infinity, 60),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(extraLarge),
+      ),
+      textStyle: GoogleFonts.leagueSpartan(
+        fontWeight: FontWeight.w600,
+        fontSize: medium,
+      ),
+    ),
+  );
+}
