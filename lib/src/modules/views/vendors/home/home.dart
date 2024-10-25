478e6a8d6ea185e0ee4cff42d157d60019ce199a
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-10-25 15:41:31 +0100

    started with home screen

--- lib/src/modules/views/vendors/home/home.dart
+++ lib/src/modules/views/vendors/home/home.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:my_street_tab/src/modules/views/vendors/home/widgets/home_header.dart';
+import 'package:my_street_tab/src/modules/views/vendors/home/widgets/order_status.dart';
+import 'package:my_street_tab/src/modules/views/vendors/home/widgets/reviews_summary.dart';
+import 'package:my_street_tab/src/utils/core/sizes.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Home"),
+    return const SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.all(smallLarge),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                // home header
+                HomeHeader(location: "Halal Lab Office"),
+                SizedBox(height: large),
+
+                // order status container
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    OrderStatus(
+                      amount: "20",
+                      title: "running orders",
+                    ),
+                    SizedBox(width: smallLarge),
+                    OrderStatus(
+                      amount: "05",
+                      title: "order request",
+                    ),
+                  ],
+                ),
+                SizedBox(height: medium),
+
+                // reviews
+                ReviewsSummary(rating: "4.9", amount: "411"),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
