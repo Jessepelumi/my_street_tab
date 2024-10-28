4bfc2ebd43555c685d9231b64e903013e5048e3b
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-10-28 16:03:10 +0100

    created the structure of the chat screen

--- lib/src/modules/views/users/chat/chat.dart
+++ lib/src/modules/views/users/chat/chat.dart
@@ -1,12 +1,111 @@
 import 'package:flutter/material.dart';
+import 'package:my_street_tab/src/modules/views/shared/custom_appbar.dart';
+import 'package:my_street_tab/src/utils/constants/color_strings.dart';
+import 'package:my_street_tab/src/utils/core/sizes.dart';
 
 class Chat extends StatelessWidget {
   const Chat({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Chat"),
+    return Scaffold(
+      appBar: CustomAppBar(title: "Chat List", isPageHeader: true),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(large),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: medium),
+                child: Text(
+                  "All Message",
+                  style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                        fontWeight: FontWeight.w700,
+                      ),
+                ),
+              ),
+
+              // message container
+              Container(
+                padding: const EdgeInsets.all(smallMedium),
+                decoration: BoxDecoration(
+                  color: white,
+                  borderRadius: BorderRadius.circular(medium),
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                              color: primary.withOpacity(0.2),
+                              borderRadius: BorderRadius.circular(extraLarge),
+                            ),
+                          ),
+                          const SizedBox(width: small),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Geopart Etdsien",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium
+                                    ?.copyWith(
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                              ),
+                              Text(
+                                "Your order just arived!",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .labelLarge
+                                    ?.copyWith(
+                                      color: darkText.withOpacity(0.55),
+                                    ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          "13:47",
+                          style:
+                              Theme.of(context).textTheme.labelMedium?.copyWith(
+                                    color: darkText.withOpacity(0.5),
+                                  ),
+                        ),
+                        const SizedBox(height: extraSmall / 2),
+                        Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 4),
+                          decoration: BoxDecoration(
+                            color: primary,
+                            borderRadius: BorderRadius.circular(extraLarge),
+                          ),
+                          child: const Center(
+                            child: Text(
+                              "3",
+                              style: TextStyle(color: white, fontSize: 10),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
