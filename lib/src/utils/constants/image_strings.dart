49f32276bc3f494e2cbee271c49eeb1190c394f4
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-10-29 10:02:50 +0100

    added new images

--- lib/src/utils/constants/image_strings.dart
+++ lib/src/utils/constants/image_strings.dart
@@ -1,6 +1,6 @@
 class ImageStrings {
   // onboarding images
-  static const String order = "assets/svg/order-onboarding.svg";
-  static const String delivery = "assets/svg/delivery-onboarding.svg";
-  static const String favorite = "assets/svg/favorite-onboarding.svg";
+  static const String order = "assets/images/order-image.png";
+  static const String delivery = "assets/images/delivery-image.png";
+  static const String favorite = "assets/images/favorites-image.png";
 }
