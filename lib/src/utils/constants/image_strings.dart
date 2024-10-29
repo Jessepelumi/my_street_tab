7758ee4e823150b163654a0016c9bbb4c2d5f12d
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-10-29 09:40:05 +0100

    added onboarding images

---
+++ lib/src/utils/constants/image_strings.dart
@@ -0,0 +1,6 @@
+class ImageStrings {
+  // onboarding images
+  static const String order = "assets/svg/order-onboarding.svg";
+  static const String delivery = "assets/svg/delivery-onboarding.svg";
+  static const String favorite = "assets/svg/favorite-onboarding.svg";
+}
