fad293e011586d433b58c5b1d6c91ab365210aaa
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-10-30 07:30:30 +0100

    added brand colors

--- lib/src/utils/constants/color_strings.dart
+++ lib/src/utils/constants/color_strings.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 const primary = Color(0xFF05145C);
 const primaryTwo = Color(0xFF0B30B2);
 const white = Color(0xFFFFFFFF);
+const black = Color(0xFF000000);
 const scaffoldWhite = Color(0xFFF5F5F5);
 const darkText = Color(0xFF0A2533);
 const red = Color(0xFFFF3D00);
+
+// brand
+const facebook = Color(0xFF395998);
+const google = Color(0xFFDB4437);
+const apple = Color(0xFF1B1F2F);
