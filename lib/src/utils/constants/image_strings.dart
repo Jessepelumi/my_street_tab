65bd2bb70d5cd5f1b5b6b05212c928594f06d02d
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-10-30 15:41:58 +0100

    added new image for location access

--- lib/src/utils/constants/image_strings.dart
+++ lib/src/utils/constants/image_strings.dart
@@ -3,4 +3,7 @@ class ImageStrings {
   static const String order = "assets/images/order-image.png";
   static const String delivery = "assets/images/delivery-image.png";
   static const String favorite = "assets/images/favorites-image.png";
+
+  // others
+  static const String locationAccess = "assets/images/location-access.png";
 }
