5b42ff40fa50c269920a52450c5ace0a2999fd29
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-11-02 09:48:40 +0100

    modified quantity button and added check button

--- lib/src/modules/views/users/cart/widgets/cart_container.dart
+++ lib/src/modules/views/users/cart/widgets/cart_container.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:my_street_tab/src/modules/views/controllers/cart_controllers.dart';
 import 'package:my_street_tab/src/modules/views/users/cart/widgets/quantity_button.dart';
 import 'package:my_street_tab/src/utils/constants/color_strings.dart';
 import 'package:my_street_tab/src/utils/core/sizes.dart';
@@ -6,24 +8,43 @@ import 'package:my_street_tab/src/utils/core/sizes.dart';
 class CartContainer extends StatelessWidget {
   const CartContainer({
     super.key,
+    required this.item,
+    required this.price,
   });
+
+  final String item, price;
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(CartControllers());
+
     return Container(
-      padding: const EdgeInsets.all(medium),
+      padding: const EdgeInsets.only(
+        top: medium,
+        bottom: medium,
+        right: medium,
+      ),
       decoration: BoxDecoration(
         color: white,
         borderRadius: BorderRadius.circular(medium),
       ),
       child: Row(
         children: [
+          Obx(
+            () => Checkbox(
+              activeColor: red.withOpacity(0.8),
+              value: controller.isChecked.value,
+              onChanged: (value) {
+                controller.toggleCheckbox();
+              },
+            ),
+          ),
           Container(
             width: 80,
             height: 80,
             decoration: BoxDecoration(
               color: primary.withOpacity(0.2),
-              borderRadius: BorderRadius.circular(extraLarge * 2),
+              borderRadius: BorderRadius.circular(smallMedium),
             ),
           ),
           const SizedBox(width: medium),
@@ -35,35 +56,25 @@ class CartContainer extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      "Veggie tomato mix",
+                      item,
                       overflow: TextOverflow.ellipsis,
                       maxLines: 1,
-                      style: Theme.of(context)
-                          .textTheme
-                          .titleMedium
-                          ?.copyWith(
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
                             color: darkText,
-                            fontWeight: FontWeight.w800,
+                            fontWeight: FontWeight.w700,
                           ),
                     ),
                     const SizedBox(height: extraSmall),
                     Text(
-                      "\$24.00",
-                      style: Theme.of(context)
-                          .textTheme
-                          .titleSmall
-                          ?.copyWith(
+                      "\$$price",
+                      style: Theme.of(context).textTheme.titleSmall?.copyWith(
                             color: red,
                           ),
                     ),
                   ],
                 ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    QuantityButton(),
-                  ],
-                ),
+                const SizedBox(height: extraSmall),
+                const QuantityButton(),
               ],
             ),
           ),
@@ -72,4 +83,3 @@ class CartContainer extends StatelessWidget {
     );
   }
 }
-
