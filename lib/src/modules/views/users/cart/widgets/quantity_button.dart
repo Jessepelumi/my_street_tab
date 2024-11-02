313da89ec9930627418df6ea35b52ad9dd1e53cf
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-11-02 09:46:41 +0100

    button to set quantity (not completly done)

--- lib/src/modules/views/users/cart/widgets/quantity_button.dart
+++ lib/src/modules/views/users/cart/widgets/quantity_button.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:my_street_tab/src/modules/views/controllers/cart_controllers.dart';
 import 'package:my_street_tab/src/utils/constants/color_strings.dart';
 import 'package:my_street_tab/src/utils/core/sizes.dart';
 
@@ -9,40 +11,57 @@ class QuantityButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: extraSmall),
-      decoration: BoxDecoration(
-        color: primary,
-        borderRadius: BorderRadius.circular(small),
-      ),
-      child: Row(
-        children: [
-          GestureDetector(
-            onTap: () {},
-            child: const Icon(
-              Icons.remove,
-              color: white,
-              size: smallMedium,
-            ),
-          ),
-          const SizedBox(width: extraSmall),
-          Text(
-            "1",
-            style: Theme.of(context).textTheme.labelLarge?.copyWith(
-                  color: white,
+    final controller = Get.put(CartControllers());
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            GestureDetector(
+              onTap: controller.decrementQuantity,
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: darkText.withOpacity(0.2),
+                  ),
+                  borderRadius: BorderRadius.circular(extraLarge),
                 ),
-          ),
-          const SizedBox(width: extraSmall),
-          GestureDetector(
-            onTap: () {},
-            child: const Icon(
-              Icons.add,
-              color: white,
-              size: smallMedium,
+                child: const Icon(Icons.remove),
+              ),
             ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: small),
+              child: Obx(
+                () => Text(
+                  "${controller.quantity.value}",
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: controller.incrementQuantity,
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: darkText.withOpacity(0.2),
+                  ),
+                  borderRadius: BorderRadius.circular(extraLarge),
+                ),
+                child: const Icon(Icons.add),
+              ),
+            ),
+          ],
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: const Icon(
+            Icons.delete,
+            color: delete,
+            size: 20,
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
