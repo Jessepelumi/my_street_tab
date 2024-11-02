e411af2d1f454cf522fa6aca7c4f100ac9337104
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-11-02 09:47:34 +0100

    added action buttons

--- lib/src/modules/views/shared/custom_appbar.dart
+++ lib/src/modules/views/shared/custom_appbar.dart
@@ -9,25 +9,44 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
     required this.title,
     this.actions,
     this.isPageHeader = false,
+    this.hasAction = false,
+    this.iconData = Icons.more_horiz_rounded,
+    this.color = darkText,
+    this.action,
   });
 
   final String title;
   final List<Widget>? actions;
-  final bool isPageHeader;
+  final bool isPageHeader, hasAction;
+  final IconData? iconData;
+  final Color? color;
+  final VoidCallback? action;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: small),
+      padding: const EdgeInsets.symmetric(horizontal: large),
       child: AppBar(
         backgroundColor: scaffoldWhite,
         automaticallyImplyLeading: false,
         leading: isPageHeader == false
-            ? GestureDetector(
-                onTap: () => Get.back(),
-                child: const Icon(
-                  Icons.arrow_back_ios_new_rounded,
-                  color: darkText,
+            ? Padding(
+                padding: const EdgeInsets.all(extraSmall * 1.5),
+                child: GestureDetector(
+                  onTap: () => Get.back(),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: darkText.withOpacity(0.2),
+                      ),
+                      borderRadius: BorderRadius.circular(extraLarge),
+                    ),
+                    child: const Icon(
+                      Icons.arrow_back_ios_new_rounded,
+                      size: 20,
+                      color: darkText,
+                    ),
+                  ),
                 ),
               )
             : null,
@@ -35,10 +54,30 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
           title,
           style: Theme.of(context).textTheme.titleMedium?.copyWith(
                 color: darkText,
-                fontWeight: FontWeight.w800,
+                fontWeight: FontWeight.w600,
               ),
         ),
         centerTitle: true,
+        actions: [
+          hasAction == true
+              ? GestureDetector(
+                  onTap: action,
+                  child: Container(
+                    padding: const EdgeInsets.all(small),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: darkText.withOpacity(0.2),
+                      ),
+                      borderRadius: BorderRadius.circular(extraLarge),
+                    ),
+                    child: Icon(
+                      iconData,
+                      color: color,
+                    ),
+                  ),
+                )
+              : const SizedBox.shrink(),
+        ],
       ),
     );
   }
