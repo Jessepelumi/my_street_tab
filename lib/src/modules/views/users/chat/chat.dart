bbdab4a15a56f73a91292d97bbbe73d42e0b55e0
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-11-05 11:27:22 +0100

    linked chat detail screen

--- lib/src/modules/views/users/chat/chat.dart
+++ lib/src/modules/views/users/chat/chat.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_street_tab/src/modules/views/shared/custom_appbar.dart';
-import 'package:my_street_tab/src/utils/constants/color_strings.dart';
+import 'package:my_street_tab/src/modules/views/users/chat/widgets/chat_container.dart';
 import 'package:my_street_tab/src/utils/core/sizes.dart';
 
 class Chat extends StatelessWidget {
@@ -9,7 +9,7 @@ class Chat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(title: "Chat List", isPageHeader: true),
+      appBar: const CustomAppBar(title: "Chat List", isPageHeader: true),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(large),
@@ -27,81 +27,7 @@ class Chat extends StatelessWidget {
               ),
 
               // message container
-              Container(
-                padding: const EdgeInsets.all(smallMedium),
-                decoration: BoxDecoration(
-                  color: white,
-                  borderRadius: BorderRadius.circular(medium),
-                ),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Row(
-                        children: [
-                          Container(
-                            height: 50,
-                            width: 50,
-                            decoration: BoxDecoration(
-                              color: primary.withOpacity(0.2),
-                              borderRadius: BorderRadius.circular(extraLarge),
-                            ),
-                          ),
-                          const SizedBox(width: small),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Geopart Etdsien",
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodyMedium
-                                    ?.copyWith(
-                                      fontWeight: FontWeight.w700,
-                                    ),
-                              ),
-                              Text(
-                                "Your order just arived!",
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .labelLarge
-                                    ?.copyWith(
-                                      color: darkText.withOpacity(0.55),
-                                    ),
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Text(
-                          "13:47",
-                          style:
-                              Theme.of(context).textTheme.labelMedium?.copyWith(
-                                    color: darkText.withOpacity(0.5),
-                                  ),
-                        ),
-                        const SizedBox(height: extraSmall / 2),
-                        Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 4),
-                          decoration: BoxDecoration(
-                            color: primary,
-                            borderRadius: BorderRadius.circular(extraLarge),
-                          ),
-                          child: const Center(
-                            child: Text(
-                              "3",
-                              style: TextStyle(color: white, fontSize: 10),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              )
+              const ChatContainer()
             ],
           ),
         ),
@@ -109,3 +35,4 @@ class Chat extends StatelessWidget {
     );
   }
 }
+
