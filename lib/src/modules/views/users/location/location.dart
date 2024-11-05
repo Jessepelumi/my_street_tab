01681bebcb961a56f06bb0edeb4c5ed144652206
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-11-05 12:15:30 +0100

    started out building the location screen

--- lib/src/modules/views/users/location/location.dart
+++ lib/src/modules/views/users/location/location.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:my_street_tab/src/modules/views/users/location/widgets/location_header.dart';
 
 class Location extends StatelessWidget {
   const Location({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Location"),
+    return const Scaffold(
+      body: Column(
+        children: [
+          LocationHeader(),
+          Expanded(
+            child: SizedBox(
+              child: Center(
+                child: Text("Map Here"),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
