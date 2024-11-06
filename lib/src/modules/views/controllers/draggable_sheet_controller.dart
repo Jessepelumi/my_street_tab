ce862c36a562accf4807e4b77d19b720f624574f
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-11-06 14:18:59 +0100

    added logic for order tracking

--- lib/src/modules/views/controllers/draggable_sheet_controller.dart
+++ lib/src/modules/views/controllers/draggable_sheet_controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:my_street_tab/src/modules/views/controllers/order_status.dart';
 
 class DraggableSheetController extends GetxController {
   final draggableController = DraggableScrollableController();
@@ -15,5 +16,29 @@ class DraggableSheetController extends GetxController {
   void onClose() {
     draggableController.dispose();
     super.onClose();
+  }
+
+  // logic for order tracking
+  var orderStatuses = [
+    OrderStatus(
+      label: 'Your order has been received',
+      isCompleted: true,
+    ),
+    OrderStatus(
+      label: 'The restaurant is preparing your food',
+      isCurrent: true,
+    ),
+    OrderStatus(label: 'Your order has been picked up for delivery'),
+    OrderStatus(label: 'Order arriving soon!'),
+  ].obs;
+
+  void updateStatus(int index) {
+    for (var i = 0; i < orderStatuses.length; i++) {
+      orderStatuses[i] = OrderStatus(
+        label: orderStatuses[i].label,
+        isCompleted: i < index,
+        isCurrent: i == index,
+      );
+    }
   }
 }
