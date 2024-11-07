7357561925dee85baa5f7c3e780885b37ca27d8a
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-11-07 18:18:27 +0100

    created a container to hold search keywords

---
+++ lib/src/modules/views/users/search/widgets/commodity_container.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:my_street_tab/src/modules/views/users/search/search_details.dart';
+import 'package:my_street_tab/src/utils/constants/color_strings.dart';
+import 'package:my_street_tab/src/utils/core/sizes.dart';
+
+class CommodityContainer extends StatelessWidget {
+  const CommodityContainer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => Get.to(() => const SearchDetails()),
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: medium,
+          vertical: small,
+        ),
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: darkText.withOpacity(0.2),
+          ),
+          borderRadius: BorderRadius.circular(large),
+        ),
+        child: Text(
+          "Burger",
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+      ),
+    );
+  }
+}
