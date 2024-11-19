acc5b121c3130b2359b8db379c4a17dbc223c6f7
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-11-19 16:52:49 +0100

    linked food details screen

--- lib/src/modules/views/shared/horizontal_container.dart
+++ lib/src/modules/views/shared/horizontal_container.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:my_street_tab/src/modules/views/users/search/food_details.dart';
 import 'package:my_street_tab/src/utils/constants/color_strings.dart';
 import 'package:my_street_tab/src/utils/core/sizes.dart';
 
@@ -9,66 +11,60 @@ class HorizontalContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(
-        color: white,
-        borderRadius: BorderRadius.circular(medium),
-      ),
-      child: Row(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(medium),
-            child: Container(
-              width: MediaQuery.of(context).size.width * 0.3,
-              height: 110,
-              decoration: BoxDecoration(
-                color: primary.withOpacity(0.2),
-                borderRadius: BorderRadius.circular(medium),
+    return GestureDetector(
+      onTap: () => Get.to(() => const FoodDetails()),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          color: white,
+          borderRadius: BorderRadius.circular(medium),
+        ),
+        child: Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(medium),
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.3,
+                height: 110,
+                decoration: BoxDecoration(
+                  color: primary.withOpacity(0.2),
+                  borderRadius: BorderRadius.circular(medium),
+                ),
               ),
             ),
-          ),
-          const SizedBox(width: small),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Cookie Sandwich",
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 1,
-                  style: Theme.of(context)
-                      .textTheme
-                      .titleMedium
-                      ?.copyWith(
-                        color: darkText,
-                        fontWeight: FontWeight.w800,
-                      ),
-                ),
-                Text(
-                  "Shortbread, chocolate turtle cookies, and red velvet.",
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 2,
-                  style: Theme.of(context)
-                      .textTheme
-                      .labelLarge
-                      ?.copyWith(
-                        color: primary.withOpacity(0.5),
-                      ),
-                ),
-                Text(
-                  "\$19.05",
-                  style: Theme.of(context)
-                      .textTheme
-                      .titleMedium
-                      ?.copyWith(
-                        color: red,
-                      ),
-                ),
-              ],
+            const SizedBox(width: small),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Cookie Sandwich",
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                          color: darkText,
+                          fontWeight: FontWeight.w800,
+                        ),
+                  ),
+                  Text(
+                    "Shortbread, chocolate turtle cookies, and red velvet.",
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                    style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                          color: primary.withOpacity(0.5),
+                        ),
+                  ),
+                  Text(
+                    "\$19.05",
+                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                          color: red,
+                        ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/src/modules/views/shared/vertical_container.dart
+++ lib/src/modules/views/shared/vertical_container.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:my_street_tab/src/modules/views/users/search/food_details.dart';
 import 'package:my_street_tab/src/utils/constants/color_strings.dart';
 import 'package:my_street_tab/src/utils/constants/icon_strings.dart';
 import 'package:my_street_tab/src/utils/core/sizes.dart';
@@ -11,114 +13,121 @@ class VerticalContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(medium),
-      width: MediaQuery.of(context).size.width * 0.52,
-      decoration: BoxDecoration(
-        color: white,
-        borderRadius: BorderRadius.circular(large),
-      ),
-      child: Stack(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                height: 130,
-                decoration: BoxDecoration(
-                  color: primary.withOpacity(0.2),
-                  borderRadius: BorderRadius.circular(medium),
+    return GestureDetector(
+      onTap: () => Get.to(() => const FoodDetails()),
+      child: Container(
+        padding: const EdgeInsets.all(medium),
+        width: MediaQuery.of(context).size.width * 0.52,
+        decoration: BoxDecoration(
+          color: white,
+          borderRadius: BorderRadius.circular(large),
+        ),
+        child: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 130,
+                  decoration: BoxDecoration(
+                    color: primary.withOpacity(0.2),
+                    borderRadius: BorderRadius.circular(medium),
+                  ),
                 ),
-              ),
-              const SizedBox(height: smallMedium),
-              Text(
-                "Healthy Taco Salad with fresh vegetable",
-                overflow: TextOverflow.ellipsis,
-                maxLines: 2,
-                style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                      color: darkText,
-                      fontWeight: FontWeight.w800,
+                const SizedBox(height: smallMedium),
+                Text(
+                  "Healthy Taco Salad with fresh vegetable",
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                        color: darkText,
+                        fontWeight: FontWeight.w800,
+                      ),
+                ),
+                const SizedBox(height: small),
+                Row(
+                  children: [
+                    Row(
+                      children: [
+                        SvgPicture.asset(
+                          IconStrings.fireOutlined,
+                          width: medium,
+                          colorFilter: ColorFilter.mode(
+                            primary.withOpacity(0.5),
+                            BlendMode.srcIn,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 3),
+                          child: Text(
+                            "12 Kcal",
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelLarge
+                                ?.copyWith(
+                                  color: primary.withOpacity(0.5),
+                                ),
+                          ),
+                        ),
+                      ],
                     ),
-              ),
-              const SizedBox(height: small),
-              Row(
-                children: [
-                  Row(
-                    children: [
-                      SvgPicture.asset(
-                        IconStrings.fireOutlined,
-                        width: medium,
-                        colorFilter: ColorFilter.mode(
-                          primary.withOpacity(0.5),
-                          BlendMode.srcIn,
-                        ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: extraSmall,
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 3),
-                        child: Text(
-                          "12 Kcal",
-                          style:
-                              Theme.of(context).textTheme.labelLarge?.copyWith(
-                                    color: primary.withOpacity(0.5),
-                                  ),
-                        ),
+                      child: Icon(
+                        Icons.stop,
+                        color: primary.withOpacity(0.5),
+                        size: small,
                       ),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: extraSmall,
                     ),
-                    child: Icon(
-                      Icons.stop,
-                      color: primary.withOpacity(0.5),
-                      size: small,
+                    Row(
+                      children: [
+                        SvgPicture.asset(
+                          IconStrings.clockOutlined,
+                          width: medium,
+                          colorFilter: ColorFilter.mode(
+                            primary.withOpacity(0.5),
+                            BlendMode.srcIn,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 3),
+                          child: Text(
+                            "25 mins",
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelLarge
+                                ?.copyWith(
+                                  color: primary.withOpacity(0.5),
+                                ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                  Row(
-                    children: [
-                      SvgPicture.asset(
-                        IconStrings.clockOutlined,
-                        width: medium,
-                        colorFilter: ColorFilter.mode(
-                          primary.withOpacity(0.5),
-                          BlendMode.srcIn,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 3),
-                        child: Text(
-                          "25 mins",
-                          style:
-                              Theme.of(context).textTheme.labelLarge?.copyWith(
-                                    color: primary.withOpacity(0.5),
-                                  ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          ),
+                  ],
+                ),
+              ],
+            ),
 
-          // favourite button
-          Positioned(
-            top: 10,
-            right: 10,
-            child: GestureDetector(
-              onTap: () {},
-              child: Container(
-                padding: const EdgeInsets.all(small / 1.5),
-                decoration: BoxDecoration(
-                  color: white,
-                  borderRadius: BorderRadius.circular(small),
+            // favourite button
+            Positioned(
+              top: 10,
+              right: 10,
+              child: GestureDetector(
+                onTap: () {},
+                child: Container(
+                  padding: const EdgeInsets.all(small / 1.5),
+                  decoration: BoxDecoration(
+                    color: white,
+                    borderRadius: BorderRadius.circular(small),
+                  ),
+                  child: SvgPicture.asset(IconStrings.heartOutlined),
                 ),
-                child: SvgPicture.asset(IconStrings.heartOutlined),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
