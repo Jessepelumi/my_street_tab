de5310b311a07a09b5ce00cd3a9b6c3ac2c56d52
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-12-06 12:27:45 +0100

    added more details to the card

--- lib/src/modules/views/users/controller/select_card_controller.dart
+++ lib/src/modules/views/users/controller/select_card_controller.dart
@@ -4,8 +4,27 @@ class SelectCardController extends GetxController {
   var selectedIndex = Rxn<int>();
 
   final RxList<Map<String, String>> cards = <Map<String, String>>[
-    {"cardName": "John Smith", "cardNumber": "1234567887654321"},
-    {"cardName": "John Smith", "cardNumber": "9876543212345678"},
+    {
+      "cardName": "John Smith",
+      "cardNumber": "1234567887654321",
+      "expiryDate": "12/28",
+      "cvv": "123",
+      "cardType": "MasterCard"
+    },
+    {
+      "cardName": "John Smith",
+      "cardNumber": "9876543212345678",
+      "expiryDate": "09/27",
+      "cvv": "123",
+      "cardType": "Paypal"
+    },
+    {
+      "cardName": "Smith John",
+      "cardNumber": "3453478965419623",
+      "expiryDate": "04/30",
+      "cvv": "123",
+      "cardType": "Stripe"
+    },
   ].obs;
 
   void selectCard(int index) {
